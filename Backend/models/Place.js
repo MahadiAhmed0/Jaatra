54bb0b6380c5bbaa484e249d7488c3a623845d2f
Golam Mahadi Ahmed <mahadiahmed.2002@gmail.com>
2026-08-17 18:38:13 +0600

    feat: add Place model

---
+++ Backend/models/Place.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const placeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    category: {
+      type: String,
+      enum: ["hotel", "restaurant", "attraction"],
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    priceRange: {
+      type: String,
+      enum: ["$", "$$", "$$$"],
+    },
+    images: [{ type: String }],
+    avgRating: {
+      type: Number,
+      default: 0,
+    },
+    reviewCount: {
+      type: Number,
+      default: 0,
+    },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Place", placeSchema);
