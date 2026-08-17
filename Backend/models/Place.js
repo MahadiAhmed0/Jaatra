ee3db39fc4e55d045ecf24661962c0daa05984de
Golam Mahadi Ahmed <mahadiahmed.2002@gmail.com>
2026-08-17 18:38:23 +0600

    feat: add lat/lng fields to Place model

--- Backend/models/Place.js
+++ Backend/models/Place.js
@@ -22,6 +22,16 @@ const placeSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    lat: {
+      type: Number,
+      min: -90,
+      max: 90,
+    },
+    lng: {
+      type: Number,
+      min: -180,
+      max: 180,
+    },
     priceRange: {
       type: String,
       enum: ["$", "$$", "$$$"],
